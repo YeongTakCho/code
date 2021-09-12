04b9dbe04357cf53b760f8de949e3c012fe10f85
YeongTakCho <andycho1120@naver.com>
2021-09-12 15:53:20 +0900

    7576-토마토 성공

--- python/백준/7576-토마토.py
+++ python/백준/7576-토마토.py
@@ -1,52 +1,40 @@
 import sys
-
 M,N=map(int,sys.stdin.readline().strip().split())
-tomato=[]
-checklist=[]
-Stop=0
-
+box=[];tomatoState=[]
+zeroNum=0;stop=0
+ii=0
 for i in range(N):
     appendList=(list(map(int,sys.stdin.readline().strip().split())))
-    tomato.append(appendList)
-    if 0 in appendList:
-        checklist.append(0)
-    else:
-        checklist.append(1)
-def findTomato():
-    global tomato,M,N
-    returnList=[]
-    for n in range(N):
-        for m in range(M):
-            if tomato[n][m]==1:
-                returnList.append([n,m])
-    return returnList
-
-def putTomato(tomatoState):
-    global tomato,M,N,Stop
-    Stop=1
+    box.append(appendList)
+    for i in range(M):
+        appendNum=appendList[i]
+        if appendNum==1:
+            tomatoState.append([ii,i])
+        elif appendNum==0:
+            zeroNum+=1
+    ii+=1
+def putTomato():
+    global box,M,N,stop,zeroNum,tomatoState
+    _tomatoState=[]
+    checkState=lambda n,m:True if n>=0 and m>=0 and n<N and m<M else False
+    stop=1
     moveList=[[1,0],[-1,0],[0,1],[0,-1]]
-    inRange=lambda small,big: True if small>=0 and small<big else False
-    for tomato1 in tomatoState:
+    for tomato in tomatoState:
         for move in moveList:
-            if inRange(tomato1[0]+move[0],N) and inRange(tomato1[1]+move[1],M):
-                if tomato[tomato1[0]+move[0]][tomato1[1]+move[1]]==0:
-                    tomato[tomato1[0]+move[0]][tomato1[1]+move[1]]=1
-                    Stop=0
-                
-def docheckList():
-    global tomato,N,checklist
-    for i in range(N):
-        if 0 in tomato[i]:
-            checklist[i]=0
-        else:
-            checklist[i]=1
+            movedN=tomato[0]+move[0]; movedM=tomato[1]+move[1]
+            if checkState(movedN,movedM):
+                inbox=box[movedN][movedM]
+                if inbox==0:
+                    box[movedN][movedM]=1
+                    _tomatoState.append([movedN,movedM])
+                    zeroNum-=1
+                    stop=0
+    tomatoState=_tomatoState
 total=0
-while 0 in checklist:
-    tomatoState=findTomato()
-    putTomato(tomatoState)
-    if Stop==1:
+while zeroNum>0:
+    putTomato()
+    if stop==1:
         total=-1
         break
-    docheckList()
     total+=1
 print(total)
