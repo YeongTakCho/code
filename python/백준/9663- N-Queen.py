665786e973575be07a2a0123895e51aea70ac726
YeongTakCho <andycho1120@naver.com>
2021-12-22 21:43:06 +0900

    알고리즘은 맞았지만 간소화하지 못했다

--- python/백준/9663- N-Queen.py
+++ python/백준/9663- N-Queen.py
@@ -1,22 +1,23 @@
+#참고: https://goplanit.site/46.%20Algorithm(9663_py)/
+def queen(n,N):
+    global result
+    if n==N:
+        result +=1
+        return
+    else:
+        for i in range(N):
+            row[n] =i
+            key= True
+            for j in range(n):
+                if row[j] == row[n] or (row[n]-n) == (row[j] - j) or (row[n] +n) == (row[j]+j):
+                    key= False
+                    break
+            if key:
+                queen(n+1,N)
+        
+s = int(input())
+result = 0
+row = [300] * s
+queen(0, s)
 
-
-def queen (data, vertical, diagonal_plus, diagonal_minus,i, N): #n = the state data manipulated
-    if i==N:
-        return 1
-    
-    val= 0
-    for j in range(N):
-        if vertical[j]:
-            if diagonal_plus[i+j]:
-                if diagonal_minus[i-j+N-1]:
-                    
-                    _data = data[:]; _data[i] = j
-                    _vertical = vertical[:]; _vertical[j] = False
-                    _diagonal_plus = diagonal_plus[:]; _diagonal_plus[i+j] = False
-                    _diagonal_minus = diagonal_minus[:]; _diagonal_minus[i-j+N-1] = False
-                    
-                    val+= queen(_data, _vertical, _diagonal_plus, _diagonal_minus, i+1, N)
-    return val
-for N in range(1,16):
-    print('%d. %d' %(N, queen([None]*N,[True]*N, [True]*(N*2-1),[True]*(N*2-1), 0, N)))
-    
+print(result)
