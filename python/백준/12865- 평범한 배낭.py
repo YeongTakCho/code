ac84146ac4083e57ec616edd10f6dc837d4baefc
YeongTakCho <andycho1120@naver.com>
2022-03-08 01:42:00 +0900

    2022/3/8

--- python/백준/12865- 평범한 배낭.py
+++ python/백준/12865- 평범한 배낭.py
@@ -2,20 +2,11 @@ import sys; read= sys.stdin.readline
 
 N,K = map(int,read().strip().split())
 s= [list(map(int,read().strip().split())) for _ in range(N)]
-s.sort(key= lambda d: (d[0],-d[1]))
+dp = [0] * (K+1)
 
-def getMAX(cnt,v,rest):
-    max = v
-    while cnt < len(s):
-        if s[cnt][0] > rest:
-            return max
-        
-        else:
-            val = getMAX(cnt+1, v+ s[cnt][1], rest - s[cnt][0])
-            if val > max:
-                max =val
-            cnt+=1
-    return max
+for w, v in s:
+    for cnt in range(K- w, -1, -1):
+        if dp[cnt] + v > dp[cnt+ w]:
+            dp[cnt + w] = dp[cnt] + v
 
-print(getMAX(0,0,K))
-    
+print(dp[K])
